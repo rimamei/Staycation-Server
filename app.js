cd7c62512e749a3a9d50ae05ed17b44428df62e5
Rima <rimameih@gmail.com>
2021-03-23 11:25:47 +0700

    Read dan update category

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+// Package untuk update data
+const methodOverride = require('method-override');
 
 // import mongoose
 const mongoose = require("mongoose");
@@ -24,6 +26,7 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(methodOverride('_method'));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -7,13 +7,20 @@ module.exports = {
 
   viewCategory: async (req, res) => {
     const category = await Category.find();
-    // console.log(category);
     res.render("admin/category/view_category", { category });
   },
 
   addCategory: async (req, res) => {
     const { name } = req.body;
     await Category.create({ name });
+    res.redirect("/admin/category");
+  },
+
+  editCategory: async (req, res) => {
+    const { id, name } = req.body;
+    const category = await Category.findOne({ _id: id });
+    category.name = name;
+    await category.save();
     res.redirect("/admin/category");
   },
 
