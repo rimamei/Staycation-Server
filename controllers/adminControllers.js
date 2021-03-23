7285ebc5a7932ce120abaf17972d4f474e82cb9a
Rima <rimameih@gmail.com>
2021-03-23 09:52:11 +0700

    View booking, bank, dan item

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -7,5 +7,16 @@ module.exports = {
         res.render('admin/category/view_category')
     },
 
+    viewBank: (req, res) => {
+        res.render('admin/bank/view_bank')
+    },
+
+    viewItem: (req, res) => {
+        res.render('admin/item/view_item')
+    },
+    
+    viewBooking: (req, res) => {
+        res.render('admin/booking/view_booking')
+    },
 
 }
--- routes/admin.js
+++ routes/admin.js
@@ -3,5 +3,8 @@ const adminController = require('../controllers/adminControllers');
 
 router.get('/dashboard', adminController.viewDashboard);
 router.get('/category', adminController.viewCategory);
+router.get('/bank', adminController.viewBank);
+router.get('/item', adminController.viewItem);
+router.get('/booking', adminController.viewBooking);
 
 module.exports = router;
