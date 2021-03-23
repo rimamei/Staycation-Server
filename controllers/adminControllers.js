f7d710a726f228b132edb3a8ab7baa44ee950f56
Rima <rimameih@gmail.com>
2021-03-23 09:17:17 +0700

    View Category dan setting table

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,5 +1,11 @@
 module.exports = {
     viewDashboard: (req, res) => {
         res.render('admin/dashboard/view_dashboard')
-    }
+    },
+    
+    viewCategory: (req, res) => {
+        res.render('admin/category/view_category')
+    },
+
+
 }
