d4cd69a99b0bc87a0ff343ae502b1fc77b63b30a
Rima <rimameih@gmail.com>
2021-03-24 18:02:50 +0700

    Delete item

--- routes/admin.js
+++ routes/admin.js
@@ -24,6 +24,7 @@ router.post('/item', uploadMultiple, adminController.addItem);
 router.get('/item/show-image/:id', adminController.showImageItem);
 router.get('/item/:id', adminController.showEditItem);
 router.put('/item/:id', uploadMultiple, adminController.editItem);
+router.delete('/item/:id/delete', adminController.deleteItem);
 
 // booking
 router.get('/booking', adminController.viewBooking);
