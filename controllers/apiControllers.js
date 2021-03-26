14be98e8cfa0270366cf94b4ae3c224a345fdd0c
Rima <rimameih@gmail.com>
2021-03-26 09:32:38 +0700

    Api landing page

--- controllers/apiControllers.js
+++ controllers/apiControllers.js
@@ -1,6 +1,72 @@
+const Item = require("../models/Item");
+const Treasure = require("../models/Activity");
+const Traveler = require("../models/Booking");
+const Category = require("../models/Category");
+
 module.exports = {
-    landingPage: (req, res) => {
-        const message = 'Hello Json';
-        res.status(200).json({ message });
+  landingPage: async (req, res) => {
+    try {
+      const mostPicked = await Item.find()
+        .select("_id title country city price unit sumBooking imageId")
+        .limit(5)
+        // Jika menggunakan .populate('imageId') maka result nya akan ada 3 objek
+        .populate({ path: "imageId", select: "_id imageUrl" });
+
+      const category = await Category.find()
+        .select("_id name")
+        .limit(3)
+        .populate({
+          path: "itemId",
+          select: "_id title country city isPopular imageUrl",
+          perDocumentLimit: 4,
+          option: { sort: { sumBooking: -1 } },
+          populate: {
+            path: "imageId",
+            select: "_id imageUrl",
+            perDocumentLimit: 1,
+          },
+        });
+
+      const traveler = await Traveler.find();
+      const treasure = await Treasure.find();
+      const city = await Item.find();
+
+      for (let i = 0; i < category.length; i++) {
+        for (let j = 0; j < category[i].itemId.length; j++) {
+          const item = await Item.findOne({ _id: category[i].itemId[j]._id });
+          item.isPopular = false;
+          await item.save();
+          if (category[i].itemId[0] === category[i].itemId[j]) {
+            item.isPopular = true;
+            await item.save();
+          }
+        }
+      }
+
+      const testimonial = {
+        _id: "asd1293uasdads1",
+        imageUrl: "/images/testimonial2.jpg",
+        name: "Happy Family",
+        rate: 4.55,
+        content:
+          "What a great trip with my family and I should try again next time soon ...",
+        familyName: "Angga",
+        familyOccupation: "Product Designer",
+      };
+
+      res.status(200).json({
+        hero: {
+          travelers: traveler.length,
+          treasures: treasure.length,
+          cities: city.length,
+        },
+        mostPicked,
+        category,
+        testimonial,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error" });
     }
-}
+  },
+};
